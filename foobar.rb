5f1699269689fbae46f1bb96a3a8a8a7aeb0e4b2
hironorinakauchi <hironorisama@gmail.com>
2018-09-24 16:17:24 -0700

    I finally did it....

--- foobar.rb
+++ foobar.rb
@@ -4,5 +4,13 @@ class Foobar
     # Class method, not an instance method
     # Call with `Foobar.baz`
     # Q4 CODE HERE
+    #a = a.map(&:to_i).each{|num| num + 2}.select{|num| num %2 == 0 and num <= 10}.inject(:+)
+    a = a.map(&:to_i)
+    sum = 0
+    a.each do |num|
+    	num += 2
+    	sum += num if num % 2 == 0 and num <= 10
+    end
+    sum
   end
 end
--- hw1.rb
+++ hw1.rb
@@ -1,16 +1,19 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
+  (a + b)**2
 
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
-
+  a = a.sort
+  a = a.map {|n| n + 1}
+  a
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
-
+  first_name + " " + last_name
 end
 
 def blockin_time(a)
@@ -49,4 +52,5 @@ def scrabble(word)
     z: 10,
   }
   # Q5 CODE HERE
+  word.split("").map{|str| values[str.to_sym]}.sum
 end
